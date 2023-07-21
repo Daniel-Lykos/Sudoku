f418c129ff7cf99cafc8d780b881f2e058ebd763
Daniel Silva <daniel.lykos@gmail.com>
2023-07-20 22:13:15 -0300

    Base da matriz passou no teste! Dando continuidade!

--- Sudoku.c
+++ Sudoku.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <time.h>
 #define N 3
 #define M 3
 int main(){
 
     int vetor[N][M]; 
-    int i, j, contador;
+    int i, j, contador = 1;
 
-    srand (time(NULL));
     for (i = 0; i < 3; i++){
         for (j = 0; j < 3; j++){
             vetor[i][j] = contador;
